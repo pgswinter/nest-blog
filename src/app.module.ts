69b430973d3cff36f70825b3369ac77bd2f570bb
pgswinter <tuannanh.nguyen.freelance@gmail.com>
2020-09-28 20:58:30 +0700

    create database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    useClass: DatabaseService
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
